17c67e668ceeecf47f5e6c4afeaf81fd341cf251
unknown <arlindvisha@hotmail.com>
2023-11-03 10:55:43 +0000

    Adding milestone_3.py updates to make the function work well and check if the user input is a single character and retuns whether its in the word or not

--- milestone_3.py
+++ milestone_3.py
@@ -3,16 +3,21 @@ import random
 word_list = ['Apple', 'Bananna', 'Grape', 'Orange', 'Peach']
 word = random.choice(word_list)
 
-while True:
-    guess = input('Guess the fruit (enter the first letter only):\n').strip().lower()  # Convert to lowercase for case-insensitive comparison
-
-    if len(guess) == 1 and guess.isalpha():  # Check if the input is a single alphabetical character
-        break
+def check_guess(guess):
+    if guess in word:
+        print('Good Guess')
     else:
-        print('Invalid input. Please, enter a single alphabetical character.')
+        print(f'Sorry, {guess} is not in word. Try again')
 
-if guess.lower() in word.lower():
-    print('Good Guess')
-else:
-    print(f'Sorry, {guess} is not the first letter of the word. Try again')
+
+def ask_for_input():
+    while True:
+        guess = input('Guess the fruit (enter the first letter only):\n').lower()  # Convert to lowercase for case-insensitive comparison
+        if len(guess) == 1 and guess.isalpha():  # Check if the input is a single alphabetical character
+            break
+        else:
+            print('Invalid input. Please, enter a single alphabetical character.')
+    check_guess(guess)
+
+ask_for_input()
 
